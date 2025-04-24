f186667ff690533e6d88f05d8c26e78a8e5a24a1
feng <159880903+fengchangt@users.noreply.github.com>
2025-04-24 10:10:06 +0800

    Add files via upload

--- Cmake/4ShareLibDemo/StandDemo/src/lib.cpp
+++ Cmake/4ShareLibDemo/StandDemo/src/lib.cpp
@@ -1,1 +1,6 @@
+#include "lib.h"
 
+void Helloprintfunc(void)
+{
+	std::cout << "This My lib !!! " << std::endl;
+}
---
+++ Cmake/4ShareLibDemo/StandDemo/src/lib_1.cpp
@@ -0,0 +1,6 @@
+#include "lib.h"
+
+void Helloprintfunc_1(void)
+{
+	std::cout << "This My lib_1 !!! " << std::endl;
+}
